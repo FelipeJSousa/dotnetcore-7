fb64d86d1c797a6456506136bc38059c8d7f4259
Felipe Jose Cardoso De Sousa (SISTEMAS DE TI) <felipe.sousa@unimedprudente.com.br>
2023-01-04 14:04:25 -0300

    feat(Model): Add User

---
+++ dotnetcore-7/dotnetcore-7/Model/User.cs
@@ -0,0 +1,10 @@
+namespace dotnetcore_7.Model;
+
+public class User
+{
+    public string NickName { get; set; }
+        
+    public string Name { get; set; }
+        
+    public string SureName { get; set; }
+}
